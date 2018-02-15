2782f10d5f0d1b7f5e6e39d16ede886cb9f2f83a
Leandro <le.inacio.souza@gmail.com>
2018-02-15 17:04:24 -0200

    Fixing icons issue

--- src/components/user/List.vue
+++ src/components/user/List.vue
@@ -17,7 +17,14 @@
         <td class="text-xs-right">{{ props.item.fat }}</td>
         <td class="text-xs-right">{{ props.item.carbs }}</td>
         <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
+        <td class="justify-center layout px-0">
+          <v-btn icon class="mx-0" @click="editItem(props.item)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
+        </td>
       </template>
     </v-data-table>
 
