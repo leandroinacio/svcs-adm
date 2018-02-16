5974e8699e072536dff5ea21bc739f6146b865b0
Leandro Inacio <le.inacio.souza@gmail.com>
2018-02-16 11:40:15 -0800

    Preparing for merge

--- src/components/menu/MainMenu.vue
+++ src/components/menu/MainMenu.vue
@@ -1,30 +1,26 @@
 <template>
-  <v-layout row justify-center>
-    <v-flex xs6>
-      <v-navigation-drawer permanent>
-        <v-toolbar flat>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-title class="title">
-                Application
-              </v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-        <v-divider></v-divider>
-        <v-list dense class="pt-0">
-          <!-- <v-list-tile v-for="item in items" :key="item.title" @click="handleSelect">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile> -->
-        </v-list>
-      </v-navigation-drawer>
-    </v-flex>
-  </v-layout>
+  <v-navigation-drawer permanent>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Application
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-divider></v-divider>
+    <v-list dense class="pt-0">
+      <!-- <v-list-tile v-for="item in items" :key="item.title" @click="handleSelect">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile> -->
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
--- src/components/user/List.vue
+++ src/components/user/List.vue
@@ -1,32 +1,40 @@
 <template>
   <v-container fluid>
+    
+    <v-layout row justify-start>
 
-    <main-menu></main-menu> 
+      <v-flex xs3>
+        <main-menu></main-menu> 
+      </v-flex>
 
-    <v-btn color="success">Novo Usuario</v-btn>
+      <v-flex xs8>
+        <v-btn color="success">Novo Usuario</v-btn>
 
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      hide-actions
-      class="elevation-1">
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          hide-actions
+          class="elevation-1">
 
-      <template slot="items" slot-scope="props">
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="justify-center layout px-0">
-          <v-btn icon class="mx-0" @click="editItem(props.item)">
-            <v-icon color="teal">edit</v-icon>
-          </v-btn>
-          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
-            <v-icon color="pink">delete</v-icon>
-          </v-btn>
-        </td>
-      </template>
-    </v-data-table>
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.calories }}</td>
+            <td class="text-xs-right">{{ props.item.fat }}</td>
+            <td class="text-xs-right">{{ props.item.carbs }}</td>
+            <td class="text-xs-right">{{ props.item.protein }}</td>
+            <td class="justify-center layout px-0">
+              <v-btn icon class="mx-0" @click="editItem(props.item)">
+                <v-icon color="blue">edit</v-icon>
+              </v-btn>
+              <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+                <v-icon color="red">delete</v-icon>
+              </v-btn>
+            </td>
+          </template>
+        </v-data-table>
+
+      </v-flex>
+    </v-layout> 
 
   </v-container>
 </template>
